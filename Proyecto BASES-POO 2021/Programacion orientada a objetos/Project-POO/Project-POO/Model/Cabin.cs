1db10d6731b9199c541f7b17a686b4eddc8e3d11
GerardoAmaya <00174220@uca.edu.sv>
2021-06-24 18:12:39 -0600

    creacion de context, generando models

---
+++ Proyecto BASES-POO 2021/Programacion orientada a objetos/Project-POO/Project-POO/Model/Cabin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project_POO
+{
+    public partial class Cabin
+    {
+        public Cabin()
+        {
+            Consults = new HashSet<Consult>();
+            Employees = new HashSet<Employee>();
+            Gestorxcabins = new HashSet<Gestorxcabin>();
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Adress { get; set; }
+
+        public virtual ICollection<Consult> Consults { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+        public virtual ICollection<Gestorxcabin> Gestorxcabins { get; set; }
+    }
+}
---
+++ Proyecto BASES-POO 2021/Programacion orientada a objetos/Project-POO/Project-POO/Model/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project_POO
+{
+    public partial class Register
+    {
+        public int Id { get; set; }
+        public int IdGestor { get; set; }
+        public string Gestor { get; set; }
+        public string LoginDay { get; set; }
+        public string LoginTime { get; set; }
+
+        public virtual Gestor IdGestorNavigation { get; set; }
+    }
+}

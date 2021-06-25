66c8ee268b1636c20e0affb51c44f7f2ad7555ec
romerronal <romerronal@gmail.com>
2021-06-25 16:39:52 -0600

    adding new files of database signature

---
+++ Proyecto BASES-POO 2021/Programacion orientada a objetos/Project-POO/Project-POO/Form/AppointmentForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Project_POO
+{
+    public partial class AppointmentForm : Form
+    {
+        public AppointmentForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Hide();
+            var ventana = new AppointmentForm2();
+            ventana.Show();
+        }
+    }
+}
---
+++ Proyecto BASES-POO 2021/Programacion orientada a objetos/Project-POO/Project-POO/Form/AppointmentForm2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Project_POO
+{
+    public partial class AppointmentForm2 : Form
+    {
+        public AppointmentForm2()
+        {
+            InitializeComponent();
+        }
+
+        private void lblChange_Click(object sender, EventArgs e)
+        {
+            Hide();
+            var ventana = new AppointmentForm();
+            ventana.Show();
+        }
+    }
+}

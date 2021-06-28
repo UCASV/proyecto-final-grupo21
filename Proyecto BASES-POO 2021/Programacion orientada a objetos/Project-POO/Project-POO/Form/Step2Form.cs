39e91eefb464c48fac3fd99f81869886c9b215fa
SamMontes <sammontezz.sm@gmail.com>
2021-06-27 19:01:12 -0600

    Adding Step1&2

---
+++ Proyecto BASES-POO 2021/Programacion orientada a objetos/Project-POO/Project-POO/Form/Step2Form.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Project_POO.Context;
+using Project_POO.Model;
+using Project_POO.Context;
+using System.Linq;
+
+namespace Project_POO
+{
+    public partial class Step2Form : Form
+    {
+        public Step2Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" && textBox2.Text == "")
+                MessageBox.Show("No se han registrado la fecha y hora de la primera dosis", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                var db = new VaccinationContext();
+                List<VaccinationProcess> processes = db.VaccinationProcesses.ToList();
+                VaccinationProcess process = new VaccinationProcess();
+                {
+                    process.VaccunationDay = textBox1.Text;
+                    process.VaccunationTime = textBox2.Text;
+                };
+                processes.Add(process);
+                db.SaveChanges();
+                MessageBox.Show("Se registró la fecha y el dia exitosamente", "Registro exitoso", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                Hide();
+            }
+        }
+    }
+}

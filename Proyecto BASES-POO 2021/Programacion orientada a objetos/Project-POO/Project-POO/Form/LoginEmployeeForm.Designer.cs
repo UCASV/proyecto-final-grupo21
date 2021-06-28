4fd1aea1169e126c50ceda3aa6fb54d3e363baae
romerronal <romerronal@gmail.com>
2021-06-28 00:54:47 -0600

    updating local repo

--- Proyecto BASES-POO 2021/Programacion orientada a objetos/Project-POO/Project-POO/Form/LoginEmployeeForm.Designer.cs
+++ Proyecto BASES-POO 2021/Programacion orientada a objetos/Project-POO/Project-POO/Form/LoginEmployeeForm.Designer.cs
@@ -46,9 +46,10 @@ namespace Project_POO.Form
             // btnLog
             // 
             this.btnLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.btnLog.Location = new System.Drawing.Point(225, 259);
+            this.btnLog.Location = new System.Drawing.Point(169, 210);
+            this.btnLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLog.Name = "btnLog";
-            this.btnLog.Size = new System.Drawing.Size(148, 56);
+            this.btnLog.Size = new System.Drawing.Size(111, 46);
             this.btnLog.TabIndex = 0;
             this.btnLog.Text = "Login";
             this.btnLog.UseVisualStyleBackColor = true;
@@ -56,18 +57,20 @@ namespace Project_POO.Form
             // 
             // txtEmployee
             // 
-            this.txtEmployee.Location = new System.Drawing.Point(225, 112);
+            this.txtEmployee.Location = new System.Drawing.Point(169, 91);
+            this.txtEmployee.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtEmployee.Multiline = true;
             this.txtEmployee.Name = "txtEmployee";
-            this.txtEmployee.Size = new System.Drawing.Size(291, 34);
+            this.txtEmployee.Size = new System.Drawing.Size(219, 28);
             this.txtEmployee.TabIndex = 1;
             // 
             // lblName
             // 
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.lblName.Location = new System.Drawing.Point(69, 102);
+            this.lblName.Location = new System.Drawing.Point(52, 83);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(150, 53);
+            this.lblName.Size = new System.Drawing.Size(112, 43);
             this.lblName.TabIndex = 2;
             this.lblName.Text = "Nombre:";
             this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -75,54 +78,59 @@ namespace Project_POO.Form
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label1.Location = new System.Drawing.Point(69, 174);
+            this.label1.Location = new System.Drawing.Point(52, 141);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 53);
+            this.label1.Size = new System.Drawing.Size(112, 43);
             this.label1.TabIndex = 3;
             this.label1.Text = "ID:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(225, 183);
+            this.txtId.Location = new System.Drawing.Point(169, 149);
+            this.txtId.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtId.Multiline = true;
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(279, 34);
+            this.txtId.Size = new System.Drawing.Size(210, 28);
             this.txtId.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label2.Location = new System.Drawing.Point(115, 40);
+            this.label2.Location = new System.Drawing.Point(86, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(340, 54);
+            this.label2.Size = new System.Drawing.Size(255, 44);
             this.label2.TabIndex = 5;
             this.label2.Text = "Bienvenido, Ingrese sus datos";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(171, 343);
+            this.pictureBox1.Location = new System.Drawing.Point(128, 279);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(239, 103);
+            this.pictureBox1.Size = new System.Drawing.Size(179, 84);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // dtmPicker
             // 
-            this.dtmPicker.Location = new System.Drawing.Point(438, 285);
+            this.dtmPicker.Location = new System.Drawing.Point(328, 232);
+            this.dtmPicker.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtmPicker.Name = "dtmPicker";
-            this.dtmPicker.Size = new System.Drawing.Size(107, 22);
+            this.dtmPicker.Size = new System.Drawing.Size(81, 20);
             this.dtmPicker.TabIndex = 7;
             this.dtmPicker.Visible = false;
             // 
             // LoginEmployeeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (136)))), ((int) (((byte) (182)))));
-            this.ClientSize = new System.Drawing.Size(582, 477);
+            this.ClientSize = new System.Drawing.Size(436, 388);
             this.Controls.Add(this.dtmPicker);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
@@ -132,6 +140,7 @@ namespace Project_POO.Form
             this.Controls.Add(this.txtEmployee);
             this.Controls.Add(this.btnLog);
             this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LoginEmployeeForm";
             this.Text = "LoginEmployee";
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
